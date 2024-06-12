202a54a6650b32dafbe559d6cd94fa9dd92d5c93
Trần Tấn Lộc <20130312@st.hcmuaf.edu.vn>
2024-06-12 13:19:08 +0700

    update header

--- news-web/src/index.js
+++ news-web/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider } from 'react-router-dom';
+import router from "./components/router/WebRouter";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App>
+      <RouterProvider router={router}></RouterProvider>
+    </App>
   </React.StrictMode>
 );
 
