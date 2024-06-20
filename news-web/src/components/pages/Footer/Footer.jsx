dbbdf516d7bfbfa75e20391f4871f4e5f4fc0128
Trần Tấn Lộc <20130312@st.hcmuaf.edu.vn>
2024-06-20 18:22:12 +0700

    update Footer 2

--- news-web/src/components/pages/Footer/Footer.jsx
+++ news-web/src/components/pages/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import '../../css/footer.css';
+import LogoImage from '../../images/tintuc.png';
 import MenuFooterRouter from '../../router/MenuRouter';
 
 function Footer() {
@@ -23,6 +24,25 @@ function Footer() {
           <p className='footer_title-info'>
             Thông tin của chính phủ do NLU phát hành
           </p>
+        </div>
+        <div id='footer_info'>
+          <div id='footer_info-blk1'>
+            <img src={LogoImage} className='blk1_img-logo' alt=''/>
+            <div id='name_eic-blk'>
+              <p id='name_eic-title'>
+                Tổng biên tập: 
+              </p>
+              <p id='name_eic'>
+                Trần Tấn Lộc
+              </p>
+            </div>
+          </div>
+          <div id='footer_info-blk2'>
+            <img src={LogoImage} className='blk1_img-logo' alt=''/>
+          </div>
+          <div id='footer_info-blk3'>
+            <img src={LogoImage} className='blk1_img-logo' alt=''/>
+          </div>
         </div>
       </div>
     </footer>
