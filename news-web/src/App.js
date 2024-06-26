49e896994b7f70c15d30ddd5c03b8adee379c524
Bùi Lê Trung <144335173+eoti-639@users.noreply.github.com>
2024-06-26 23:45:01 +0700

    change login form

--- news-web/src/App.js
+++ news-web/src/App.js
@@ -1,20 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
-import { Routes, Route } from "react-router-dom";
-// Page
-import HomePage from "./page/HomePage/HomePage";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import LoginForm from "./components/Login/login"
 
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
-      <Routes>
-        <Route path='/' element={<HomePage/>}/>
-      </Routes>
-      {/* <Footer/> */}
+
+     <LoginForm />
+
     </div>
   );
 }
