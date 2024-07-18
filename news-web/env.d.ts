7aa4997b55cc2efc48f270401a2c35c89d19f662
binhdtqxk <146164453+binhdtqxk@users.noreply.github.com>
2024-07-18 11:11:03 +0700

    Save current changes before rebuilding source

--- news-web/env.d.ts
+++ news-web/env.d.ts
@@ -1,9 +1,9 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-    readonly VITE_SERVER_RSS: string;
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv;
-}
+//
+// interface ImportMetaEnv {
+//     readonly VITE_SERVER_RSS: string;
+// }
+//
+// interface ImportMeta {
+//     readonly env: ImportMetaEnv;
+// }
