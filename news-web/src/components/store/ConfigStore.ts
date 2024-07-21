8740f84cdd7b114dd14d31899b1c29409cda4e8f
Trần Tấn Lộc <20130312@st.hcmuaf.edu.vn>
2024-07-21 20:52:51 +0700

    delete file

--- news-web/src/components/store/ConfigStore.ts
+++
@@ -1,6 +0,0 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import { root } from "./RootReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
-const reducer = combineReducers({ root });
-const enhancers = composeWithDevTools({});
-export const store = configureStore({ reducer, enhancers });
--- news-web/src/devTools.ts
+++
@@ -1,3 +0,0 @@
-// devTools.ts
-export const composeWithDevTools =
-  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
